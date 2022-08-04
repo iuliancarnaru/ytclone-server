a7790918fd3d3aacb42cd7a9a5722dad053b699a
iulian.carnaru <iulian@seenit.io>
2022-08-04 20:47:38 +0100

    create models

---
+++ models/Comment.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    videoId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Comment", CommentSchema);
