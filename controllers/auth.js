14921f85a05d7d056ed1cc431163228d3553a33b
iulian.carnaru <iulian@seenit.io>
2022-08-04 21:56:54 +0100

    create error handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,19 @@
-import mongoose from "mongoose";
+import * as argon2 from "argon2";
+import { createError } from "../error.js";
 import User from "../models/User.js";
 
-export const signup = async (req, res) => {
-  console.log(req.body);
-  // try {
-  //   const newUser = new User(req.body);
-  // } catch (error) {
-  //   // handle error
-  // }
+export const signup = async (req, res, next) => {
+  try {
+    const hashedPassword = await argon2.hash(req.body.password);
+
+    const newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(200).json({ message: "User has been created" });
+  } catch (error) {
+    next(error);
+  }
 };
