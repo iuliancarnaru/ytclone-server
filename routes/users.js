adcbce6e7d370d051a41e82427a17364f04fcd05
Iulian Carnaru <iulian@seenit.io>
2022-08-09 21:35:36 +0100

    user routes

--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,18 @@ import {} from "../controllers/user.js";
 
 const router = express.Router();
 
+// update user
+
+// delete user
+
+// get a user
+
+// subscribe a user
+
+// unsubscribe a user
+
+// like a video
+
+// dislike a video
+
 export default router;
